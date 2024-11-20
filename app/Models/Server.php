cba07fdf6625bb985828f50f2fdfa5e69c7495f2
Eder-Alfredo21 <116681348+Eder-Alfredo21@users.noreply.github.com>
2024-11-20 13:09:43 -0500

    Consulta para clientes y comprobantes por cliente

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -59,4 +59,63 @@ class Server extends Model
         }
         return $result;
     }
+    public function listar_clientes($search){
+        try {
+            // MAECLI - CLIENTES
+            $result = DB::connection('sqlsrv_external')->table('MAECLI')
+                ->where(function ($q) use ($search) {
+                    $q->where('CNOMCLI', 'like', '%' . $search . '%')
+                        ->orWhere('CCODCLI', 'like', '%' . $search . '%')
+                        ->orWhere('CDIRCLI', 'like', '%' . $search . '%');
+                })->where('CESTADO','=','V')->get();
+
+            }catch (\Exception $e){
+            $this->logs->insertarLog($e);
+            $result = [];
+        }
+        return $result;
+    }
+    public function listar_comprobantes_por_cliente($codigo_cliente,$search){
+        try {
+            // FACCAB - COMPROBANTES | FACDET - COMPROBANTES DETALLE | MAEART - ARTICULOS | MAEART_ADICIONALES - detalles del articulos
+            $result = DB::connection('sqlsrv_external')
+                ->table('FACCAB')
+                ->select('FACCAB.CFTD', 'FACCAB.CFNUMSER', 'FACCAB.CFNUMDOC', DB::raw('SUM(ad.CAMPO004) as total_volumen') ,DB::raw('SUM(ad.CAMPO005) as total_kg'))
+                ->join('FACDET AS cd', function ($join) {
+                    $join->on('cd.DFTD', '=', 'FACCAB.CFTD') // Condición 1
+                    ->on('cd.DFNUMSER', '=', 'FACCAB.CFNUMSER') // Condición 2
+                    ->on('cd.DFNUMDOC', '=', 'FACCAB.CFNUMDOC'); // Condición 3
+                })
+                ->join('MAEART AS a','a.ACODIGO' ,'=','cd.DFCODIGO') // Unión con artículos
+                ->join('MAEART_ADICIONALES AS ad','ad.CAMPO000' ,'=','a.ACODIGO') // Unión con artículos detalles
+                ->where('FACCAB.CFESTADO','=','V')
+                ->where('FACCAB.CFCODCLI','=',$codigo_cliente)
+                ->where(function ($q) use ($search) {
+                    $q->where('c.CNOMCLI', 'like', '%' . $search . '%')
+                        ->orWhere('c.CCODCLI', 'like', '%' . $search . '%')
+                        ->orWhere('c.CDIRCLI', 'like', '%' . $search . '%')
+                        ->orWhere('FACCAB.CFTD', 'like', '%' . $search . '%')
+                        ->orWhere('FACCAB.CFNUMDOC', 'like', '%' . $search . '%')
+                        ->orWhere('FACCAB.CFNUMSER', 'like', '%' . $search . '%');
+                })
+                ->groupBy('FACCAB.CFTD', 'FACCAB.CFNUMSER', 'FACCAB.CFNUMDOC')
+                ->limit(15)->get();
+
+            foreach ($result as $re){
+                $valornew = $re->total_kg / 1000;
+                $re->total_kg = $valornew;
+                /* -------------------------------------- */
+                $re->show = 1;
+                $validar =  DB::table('despacho_ventas')->where([['despacho_venta_cftd','=',$re->CFTD], ['despacho_venta_cfnumser','=',$re->CFNUMSER], ['despacho_venta_cfnumdoc','=',$re->CFNUMDOC],])->first();
+                if ($validar){
+                    $re->show = 0;
+                }
+            }
+        }catch (\Exception $e){
+            $this->logs->insertarLog($e);
+            $result = [];
+        }
+        return $result;
+    }
+
 }
