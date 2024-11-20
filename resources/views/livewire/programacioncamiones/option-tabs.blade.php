f0d1d4a5da95465816053c9620c816bb26e91c0b
Eder-Alfredo21 <116681348+Eder-Alfredo21@users.noreply.github.com>
2024-11-20 17:01:18 -0500

    Mejora de la vista de programación de camión.

--- resources/views/livewire/programacioncamiones/option-tabs.blade.php
+++ resources/views/livewire/programacioncamiones/option-tabs.blade.php
@@ -5,19 +5,19 @@
                 <div class="col-lg-4 col-md-4 col-sm-12 mb-2">
                     <div class="d-flex align-items-center {{$estadoTabs == 1 ? 'activeType' : 'bg-white'}}" style="cursor:pointer;border-radius: 20px;" wire:click="$set('estadoTabs', 1)">
                         <img src="{{asset('local_n.png')}}" class="tamaIm me-2" alt="">
-                        <h5 class="{{$estadoTabs == 1 ? 'text-white' : 'text-dark'}}">TRANSPORTE LOCAL</h5>
+                        <h5 class="mb-0 {{$estadoTabs == 1 ? 'text-white' : 'text-dark'}}">TRANSPORTE LOCAL</h5>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12 mb-2">
                     <div class="d-flex align-items-center {{$estadoTabs == 2 ? 'activeType' : 'bg-white'}}" style="cursor:pointer;border-radius: 20px;" wire:click="$set('estadoTabs', 2)">
                         <img src="{{asset('provi.svg')}}" class="tamaIm me-2" alt="">
-                        <h5 class="{{$estadoTabs == 2 ? 'text-white' : 'text-dark'}}">TRANSPORTE PROVINCIAL</h5>
+                        <h5 class="mb-0 {{$estadoTabs == 2 ? 'text-white' : 'text-dark'}}">TRANSPORTE PROVINCIAL</h5>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12 mb-2">
                     <div class="d-flex align-items-center {{$estadoTabs == 3 ? 'activeType' : 'bg-white'}}" style="cursor:pointer;border-radius: 20px;" wire:click="$set('estadoTabs', 3)">
                         <img src="{{asset('mix.svg')}}" class="tamaIm me-2" alt="">
-                        <h5 class="{{$estadoTabs == 3 ? 'text-white' : 'text-dark'}}">TRANSPORTE MIXTO</h5>
+                        <h5 class="mb-0 {{$estadoTabs == 3 ? 'text-white' : 'text-dark'}}">TRANSPORTE MIXTO</h5>
                     </div>
                 </div>
             </div>
