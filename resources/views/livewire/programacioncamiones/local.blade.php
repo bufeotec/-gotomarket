bdb95866709276af794d85b9a3dba4975ba57bad
Miguelvasquez17 <1456597@senati.pe>
2024-11-20 17:46:59 -0500

    Cambios

--- resources/views/livewire/programacioncamiones/local.blade.php
+++ resources/views/livewire/programacioncamiones/local.blade.php
@@ -9,7 +9,7 @@
                     <div class="card-body">
                         <div class="position-relative mb-3">
                             <input type="text" class="form-control bg-dark text-white rounded-pill ps-5 custom-placeholder"
-                                   placeholder="Buscar comprobante" wire:model="searchFactura" wire:keydown="buscar_comprobantes"
+                                   placeholder="Buscar comprobante" wire:model="searchFactura" wire:input="buscar_comprobantes"
                                    style="border: none; outline: none;" />
                             <i class="fas fa-search position-absolute"
                                style="left: 15px; top: 50%; transform: translateY(-50%); color: #bbb;"></i>
