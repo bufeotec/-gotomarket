5cc87862666d479f815ac9fe9a340913f724681d
Salomon Von Bancels <1462196@senati.pe>
2025-03-25 10:07:43 -0500

    Cambios en reportes

--- resources/views/programacion_camiones/reporte_control_documentario.blade.php
+++ resources/views/programacion_camiones/reporte_control_documentario.blade.php
@@ -13,23 +13,48 @@
     </div>
 
     <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-12">
+{{--                <div class="col-lg-6 col-md-6 col-sm-12">--}}
+{{--                    <div class="card">--}}
+{{--                        <div class="card-header">--}}
+{{--                            <h4>Reporte de Venta e Indicadores</h4>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-body">--}}
+{{--                            <div class="d-flex justify-content-end">--}}
+{{--                                <a href="{{route('Programacioncamion.reporte_ventas_indicadores')}}" class="btn btn-success">Ingresar</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+
+{{--        <div class="col-lg-6 col-md-6 col-sm-6">--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-header">--}}
+{{--                    <h4>Reporte de Estados de Comprobante</h4>--}}
+{{--                </div>--}}
+{{--                <div class="card-body">--}}
+{{--                    <div class="d-flex justify-content-end">--}}
+{{--                        <a href="{{route('Programacioncamion.reporte_estados_factura')}}" class="btn btn-success">Ingresar</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
+        <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Reporte de Venta e Indicadores</h4>
+                            <h4>Efectividad de Entrega de Pedidos</h4>
                         </div>
                         <div class="card-body">
                             <div class="d-flex justify-content-end">
-                                <a href="{{route('Programacioncamion.reporte_ventas_indicadores')}}" class="btn btn-success">Ingresar</a>
+                                <a href="{{route('Programacioncamion.reporte_estados_factura')}}" class="btn btn-success">Ingresar</a>
                             </div>
                         </div>
                     </div>
                 </div>
-
         <div class="col-lg-6 col-md-6 col-sm-6">
             <div class="card">
                 <div class="card-header">
-                    <h4>Reporte de Estados de Comprobante</h4>
+                    <h4>Reporte de Tiempos de Atención de Pedidos</h4>
                 </div>
                 <div class="card-body">
                     <div class="d-flex justify-content-end">
@@ -38,6 +63,67 @@
                 </div>
             </div>
         </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Reporte Estados de Documentos</h4>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex justify-content-end">
+                        <a href="{{route('Programacioncamion.reporte_estados_factura')}}" class="btn btn-success">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Reporte de Flete: Indicadores de Valor Transportado</h4>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex justify-content-end">
+                        <a href="{{route('Programacioncamion.reporte_estados_factura')}}" class="btn btn-success">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Reporte de Flete: Indicador de Peso Despachado</h4>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex justify-content-end">
+                        <a href="{{route('Programacioncamion.reporte_estados_factura')}}" class="btn btn-success">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Historial de Programación de Despacho</h4>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex justify-content-end">
+                        <a href="{{route('Programacioncamion.reporte_estados_factura')}}" class="btn btn-success">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Reporte de Flete: Liquidaciones Aprobadas</h4>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex justify-content-end">
+                        <a href="{{route('Programacioncamion.reporte_estados_factura')}}" class="btn btn-success">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
     <script src="{{asset('js/domain.js')}}"></script>
