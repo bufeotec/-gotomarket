cecb265af8732032dcdb462e2f30f3797aea8a53
Wagner Villasis <wagnervillasishidalgo060@gmail.com>
2025-08-11 15:35:50 -0500

    Excel auxiliar

--- app/Livewire/Programacioncamiones/Efectividadentregapedidos.php
+++ app/Livewire/Programacioncamiones/Efectividadentregapedidos.php
@@ -13,6 +13,10 @@ use Livewire\WithoutUrlPagination;
 use Livewire\WithPagination;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+
 
 class Efectividadentregapedidos extends Component
 {
@@ -87,6 +91,187 @@ class Efectividadentregapedidos extends Component
                 session()->flash('error', 'No tiene permisos para generar el reporte en excel.');
                 return;
             }
+
+//            $despachos_g = DB::table('programaciones as p')
+//                ->join('despachos as d', 'd.id_programacion', '=', 'p.id_programacion')
+//                ->whereBetween('d.despacho_fecha_aprobacion', ['2025-01-01', '2027-07-31'])
+//                ->where('d.id_tipo_servicios', 1)
+//                ->where('d.despacho_estado_aprobacion', '<>', 4)
+//                ->get();
+//
+//            // Crear archivo general
+//            $spreadsheet = new Spreadsheet();
+//            $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+//
+//            // HOJA 1: Resumen Local
+//            $sheet1 = $spreadsheet->getActiveSheet();
+//            $sheet1->setTitle('Flete Local');
+//
+//            // Encabezados
+//            $headers = [
+//                'N° OS Local',
+//                'Fecha OS Local',
+//                'Flete OS Local',
+//                'Valor Transportado por OS Local',
+//                'Peso Transportado por OS Local',
+//            ];
+//
+//            $sheet1->fromArray($headers, null, 'A1');
+//            $headerStyle = [
+//                'font' => ['bold' => true],
+//                'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER],
+//                'fill' => [
+//                    'fillType' => Fill::FILL_SOLID,
+//                    'startColor' => ['rgb' => 'FFE699']
+//                ]
+//            ];
+//            $sheet1->getStyle('A1:E1')->applyFromArray($headerStyle);
+//            $sheet1->getColumnDimension('A')->setWidth(20);
+//            $sheet1->getColumnDimension('B')->setWidth(20);
+//            $sheet1->getColumnDimension('C')->setWidth(18);
+//            $sheet1->getColumnDimension('D')->setWidth(35);
+//            $sheet1->getColumnDimension('E')->setWidth(30);
+//
+//            $row = 2;
+//            foreach ($despachos_g as $g) {
+//                $sheet1->setCellValue('A' . $row, $g->despacho_numero_correlativo);
+//                $sheet1->setCellValue('B' . $row, date('d/m/Y', strtotime($g->programacion_fecha))); //programado
+//
+//                $liquidado = DB::table('liquidacion_gastos as lg')
+//                    ->join('liquidacion_detalles as ld', 'ld.id_liquidacion_detalle', '=', 'lg.id_liquidacion_detalle')
+//                    ->where('lg.liquidacion_gasto_concepto', '=', 'costo_flete')
+//                    ->where('ld.id_despacho', '=', $g->id_despacho)
+//                    ->first();
+//
+//                if ($liquidado) {
+//                    $sheet1->setCellValue('C' . $row, $liquidado->liquidacion_gasto_monto);
+//                } else {
+//                    $sheet1->setCellValue('C' . $row, $g->despacho_costo_total);
+//                }
+//
+//                $detalle = DB::table('despacho_ventas as dv')
+//                    ->join('guias as g', 'g.id_guia', '=', 'dv.id_guia')
+//                    ->where('dv.id_despacho', '=', $g->id_despacho)
+//                    ->get();
+//
+//                $sum = 0;
+//                foreach ($detalle as $d) {
+//                    if (in_array($d->guia_estado_aprobacion, [11, 12])) {
+//                        $sum += 0;
+//                    } else {
+//                        $sum += $d->guia_importe_total_sin_igv;
+//                    }
+//                }
+//
+//                $sheet1->setCellValue('D' . $row, $sum);
+//                $sheet1->setCellValue('E' . $row, $g->despacho_peso);
+//                $row++;
+//            }
+//
+//            // HOJA 2: Resumen Nacional
+//            $sheet2 = new Worksheet($spreadsheet, 'Flete Provincia');
+//            $spreadsheet->addSheet($sheet2);
+//
+//            /*$sheet2->setCellValue('A1', 'Este es un ejemplo de la hoja nacional');
+//            $sheet2->getStyle('A1')->getFont()->setBold(true);
+//            $sheet2->getColumnDimension('A')->setWidth(50);*/
+//
+//            // Encabezados
+//            $headers2 = [
+//                'N° OS Local',
+//                'Fecha OS Local',
+//                'Departamento de Entrega',
+//                'Provincia de Entrega',
+//                'Distrito de Entrega',
+//                'Flete OS Provincia',
+//                'Valor Transportado por OS Local',
+//                'Peso Transportado por OS Local',
+//            ];
+//
+//            $sheet2->fromArray($headers2, null, 'A1');
+//            $headerStyle2 = [
+//                'font' => ['bold' => true],
+//                'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER],
+//                'fill' => [
+//                    'fillType' => Fill::FILL_SOLID,
+//                    'startColor' => ['rgb' => 'FFE699']
+//                ]
+//            ];
+//            $sheet2->getStyle('A1:H1')->applyFromArray($headerStyle2);
+//            $sheet2->getColumnDimension('A')->setWidth(20);
+//            $sheet2->getColumnDimension('B')->setWidth(20);
+//            $sheet2->getColumnDimension('C')->setWidth(30);
+//            $sheet2->getColumnDimension('D')->setWidth(35);
+//            $sheet2->getColumnDimension('E')->setWidth(30);
+//            $sheet2->getColumnDimension('F')->setWidth(30);
+//            $sheet2->getColumnDimension('G')->setWidth(30);
+//            $sheet2->getColumnDimension('H')->setWidth(30);
+//
+//            $despachos_pro = DB::table('programaciones as p')
+//                ->join('despachos as d', 'd.id_programacion', '=', 'p.id_programacion')
+//
+//                ->join('departamentos as depa', 'd.id_departamento', '=', 'depa.id_departamento')
+//                ->join('provincias as pro', 'd.id_provincia', '=', 'pro.id_provincia')
+//                ->join('distritos as di', 'd.id_distrito', '=', 'di.id_distrito')
+//                ->where('d.id_tipo_servicios', 2)
+//                ->where('d.despacho_estado_aprobacion', '<>', 4)
+//                ->whereBetween('d.despacho_fecha_aprobacion', ['2025-01-01', '2027-07-31'])
+//
+//                ->get();
+//
+//            /*$despachos_pro = DB::table('despachos as d')
+//                ->join('departamentos as depa', 'd.id_departamento', '=', 'depa.id_departamento')
+//                ->join('provincias as pro', 'd.id_provincia', '=', 'pro.id_provincia')
+//                ->join('distritos as di', 'd.id_distrito', '=', 'di.id_distrito')
+//                ->whereBetween('d.despacho_fecha_aprobacion', ['2025-01-01', '2027-07-31'])
+//                ->where('d.id_tipo_servicios', 2)
+//                ->where('d.despacho_estado_aprobacion', '<>', 4)
+//                ->get();*/
+//
+//            $row2 = 2;
+//            foreach ($despachos_pro as $dp){
+//                $sheet2->setCellValue('A' . $row2, $dp->despacho_numero_correlativo);
+//                $sheet2->setCellValue('B' . $row2, date('d/m/Y', strtotime($dp->programacion_fecha)));
+//                $sheet2->setCellValue('C' . $row2, $dp->departamento_nombre);
+//                $sheet2->setCellValue('D' . $row2, $dp->provincia_nombre);
+//                $sheet2->setCellValue('E' . $row2, $dp->distrito_nombre);
+//                $sheet2->setCellValue('F' . $row2, $dp->despacho_costo_total);
+//
+//                $detalle = DB::table('despacho_ventas as dv')
+//                    ->join('guias as g', 'g.id_guia', '=', 'dv.id_guia')
+//                    ->where('dv.id_despacho', '=', $dp->id_despacho)
+//                    ->get();
+//
+//                $sum = 0;
+//                foreach ($detalle as $d) {
+//                    if (in_array($d->guia_estado_aprobacion, [11, 12])) {
+//                        $sum += 0;
+//                    } else {
+//                        $sum += $d->guia_importe_total_sin_igv;
+//                    }
+//                }
+//
+//                $sheet2->setCellValue('G' . $row2, $sum);
+//                $sheet2->setCellValue('H' . $row2, $dp->despacho_peso);
+//                $row2++;
+//            }
+//
+//            // OPCIONAL: Hoja activa al abrir
+//            $spreadsheet->setActiveSheetIndexByName('Flete Local');
+//
+//            // Descargar archivo
+//            $nombre_excel = "REPORTE AVANCE INTRANET.xlsx";
+//            return response()->stream(
+//                function () use ($writer) {
+//                    $writer->save('php://output');
+//                },
+//                200,
+//                [
+//                    'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+//                    'Content-Disposition' => 'attachment; filename="' . $nombre_excel . '"',
+//                ]
+//            );
+
 
             // Construir la consulta base similar a la vista
             $query = DB::table('programaciones as p')
